3d1286366e69165e6687d7fac7b16ff14dba92ff
yangfeng <Lz2013@live.cn>
2016-10-23 15:16:54 +0800

    Create

---
+++ testGit/testGit.cpp
@@ -0,0 +1,14 @@
+#include "testGit.h"
+#include <iostream>
+
+int testGit()
+{
+	std::cout << "My first Git project." << std::endl;
+}
+
+int main()
+{
+	testGit();
+
+	system("pause");
+}
---
+++ testGit/testGit.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define PROGRAM_NAME		MY_FIRST_GIT_PROJECT
+
+int testGit();
